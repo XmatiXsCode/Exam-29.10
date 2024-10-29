bd96922d3d77c3beb793e0317e4701a9b2b4eb18
XmatiXsCode <133199964+XmatiXsCode@users.noreply.github.com>
2024-10-29 11:21:50 +0100

    Correct file upload

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Program liczący samogłoski.");
+Console.Write("Podaj ciąg znaków: ");
+string string_of_characters = Console.ReadLine();
+Console.WriteLine($"Liczba samogłosek w ciągu znaków: {Tools.CheckVowels(string_of_characters)}.");
+Console.WriteLine("Program naprawiający zdanie.");
+Console.Write("Podaj ciąg znaków: ");
+string sentence = Console.ReadLine();
+Console.WriteLine($"Naprawione zdanie: {Tools.FixSentence(sentence)}.");
